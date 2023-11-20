2dcfffaf2ce8b9ceba0a3e102ca5f0703b26db98
reinatch <reinatchnatch@gmail.com>
2023-11-20 20:24:03 +0000

    a

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const isProd = process.env.NODE_ENV;
 console.log(isProd);
 const nextConfig = {
   // basePath: "",
-  //   basePath: !isProd == "production" ? "" : "/reinatch",
-  //   output: "export",
-  //   distDir: "dist",
+  basePath: !isProd == "production" ? "" : "/reinatch",
+  output: "export",
+  distDir: "dist",
 };
 
 module.exports = nextConfig;
